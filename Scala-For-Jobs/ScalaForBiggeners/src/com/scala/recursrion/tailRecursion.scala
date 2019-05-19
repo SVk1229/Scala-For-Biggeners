9cd5f9cb4eaca4a9692ef25d8519b944a170a889
svk12 <svk1229@gmail.com>
2019-05-19 13:36:45 +0530

    tail recursion - svk

---
+++ Scala-For-Jobs/ScalaForBiggeners/src/com/scala/recursrion/tailRecursion.scala
@@ -0,0 +1,12 @@
+object tailRecursion extends App {
+
+  def Trecursion(x: Int, accumlator: BigInt): BigInt = {
+    if (x <= 1) {
+      accumlator
+    } else {
+      Trecursion(x - 1, x * accumlator)
+    }
+    
+  }
+println(Trecursion(9000,1))
+}
